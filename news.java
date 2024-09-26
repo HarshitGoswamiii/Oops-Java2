d079ab31730cd665caf667a81fafd91092827bb1
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-26 21:14:10 +0530

    update

--- news.java
+++ news.java
@@ -1,9 +1,9 @@
 import com.CodeWithHarry.*;
 import com.CodeWithHarry.Shape.Rectangle;
 
-public class news{
+public class news {
     public static void main(String[] args) {
         Rectangle rc = new Rectangle(8, 5);
-        
+        rc.Area();
     }
 }
