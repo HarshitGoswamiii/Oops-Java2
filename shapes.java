f733aa83c1e2a0c23ab2dc4a5c9d2a535a0c61b1
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-26 22:02:22 +0530

    update

--- shapes.java
+++ shapes.java
@@ -1,4 +1,4 @@
-class Square{                           //Class for Square
+class Square { // Class for Square
     int side;
 
     public int GetSide(int side) {
@@ -11,43 +11,51 @@ class Square{                           //Class for Square
 
     public void Area() {
         System.out.println(this.side * this.side);
-    }   
+    }
 }
 
-class Rectangle{                           //Class for Rectangle
-    int length,breadth;
+class Rectangle { // Class for Rectangle
+    int length, breadth;
 
     public int GetLength(int length) {
         return this.length;
     }
+
     public int GetBreadth(int breadth) {
         return this.breadth;
     }
+
     public void SetLength(int length) {
         this.length = length;
     }
+
     public void SetBreadth(int breadth) {
         this.breadth = breadth;
     }
 
     public void Area() {
         System.out.println(this.length * this.breadth);
-    }   
-}
-class Circle{
-    int radius;
-    public void GetRadius(int radius){
-        this.radius=radius;
     }
-    public int SetRadius(int radius){
+}
+
+class Circle { // Class For Circle
+    int radius;
+
+    public void GetRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int SetRadius(int radius) {
         return this.radius;
     }
-    public void Area(){
-        System.out.println(Math.PI*radius*radius);
+
+    public void Area() {
+        System.out.println(Math.PI * radius * radius);
     }
 }
+
 public class Shapes {
     public static void main(String[] args) {
-        
+
     }
 }
