7365447ddc314a0fdb2fa55edc40524eb0d8f728
Harshit Goswami <harshitgoswami244@gmail.com>
2024-09-27 19:17:40 +0530

    update

--- test.java
+++
@@ -1,8 +0,0 @@
-import com.CodeWithHarry.Shape.*;
-public class test {
-    public static void main(String[] args){
-        Circle Sc = new Circle();
-        Sc.SetRadius(3);
-        System.out.println(Sc.Area());
-    }
-}
