cbb9ed94241375864796f599ac91a4376970a790
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-19 19:27:19 +0530

    update

---
+++ arr.java
@@ -0,0 +1,16 @@
+public class arr {
+    public static void main(String[] args) {
+
+        // Question 1
+        // float sum = 0;
+        // float[] arr = { 12.5f, 12.4f, 4.5f, 5.1f, 5.6f };
+        // for (int i = 0; i < arr.length; i++) {
+        //     sum += arr[i];
+        // }
+        // System.out.println(sum);
+
+        // Question 2
+        
+
+    }
+}
