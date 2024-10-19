7f2ac072ad969a86e8837e2342bcf64d3327b355
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-19 19:53:06 +0530

    update

--- arr.java
+++ arr.java
@@ -5,21 +5,20 @@ public class arr {
         // float sum = 0;
         // float[] arr = { 12.5f, 12.4f, 4.5f, 5.1f, 5.6f };
         // for (int i = 0; i < arr.length; i++) {
-        //     sum += arr[i];
+        // sum += arr[i];
         // }
         // System.out.println(sum);
 
         // Question 2
-        int[] arr2 = {2,1,33,43,11,33};  
-        int find=3;      
-        System.out.println("Number : "+find);
+        int[] arr2 = { 2, 1, 33, 43, 11, 33 };
+        int find = 3;
+        boolean found = false;
+        System.out.println("Number : " + find);
         for (int i = 0; i < arr2.length; i++) {
-            if(find==arr2[i]){
+            if (find == arr2[i]) {
                 System.out.println("Found");
+                found = true;
                 break;
-            }
-            else{
-                System.out.println("Not Found");
             }
         }
     }
