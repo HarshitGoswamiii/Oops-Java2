d5ae44d3f863bb5e6c440394e6fecc72542471ab
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-19 18:55:00 +0530

    update

--- Practice1.java
+++ Practice1.java
@@ -21,7 +21,18 @@ public class Practice1 {
         }
     }
     public static void Pascal(){
-        
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        for (int i = 3; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
@@ -29,6 +40,8 @@ public class Practice1 {
         // Pattern Reverse
         // Pattern1();
 
-        Pattern2();
+        // Pattern2();
+
+        Pascal();
     }
 }
