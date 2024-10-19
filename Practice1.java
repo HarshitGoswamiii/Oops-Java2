b544cfc7a46893a6b06c9fe1c5b0ec41baad9d73
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-19 18:19:58 +0530

    update

--- Practice1.java
+++ Practice1.java
@@ -3,9 +3,9 @@ public class Practice1 {
         // Pattern Reverse
         for (int i = 5; i >= 0; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+                System.out.print(" * ");
             }
-            System.out.println("");
+            System.out.println(" ");
         }
     }
 }
