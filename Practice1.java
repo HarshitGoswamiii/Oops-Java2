a0ab3f79608b12f3e16617e2132d823964baa437
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-19 18:13:20 +0530

    update

---
+++ Practice1.java
@@ -0,0 +1,11 @@
+public class Practice1 {
+    public static void main(String[] args) {
+        // Pattern Reverse
+        for (int i = 0; i < 5; i++) {
+            for(int j = 0;j>i;j++){
+                System.out.println("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
