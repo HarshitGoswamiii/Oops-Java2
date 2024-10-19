d410409c90f168755e0d9ae80a8ee08f0cdf2abd
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-19 18:27:02 +0530

    update

--- Practice1.java
+++ Practice1.java
@@ -1,11 +1,20 @@
 public class Practice1 {
-    public static void main(String[] args) {
-        // Pattern Reverse
+    public static void Pattern1(){ //Right Triangle and Right Downward Triangle
         for (int i = 5; i >= 0; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(" * ");
+                System.out.print("* ");
             }
-            System.out.println(" ");
+            System.out.println("");
         }
+    }
+    public static void Pattern2(){
+        
+    }
+
+    
+    public static void main(String[] args) {
+        
+        // Pattern Reverse
+        Pattern1();
     }
 }
