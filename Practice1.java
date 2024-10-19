8cafa9f264ca8d6c14e9da9e7bc9b4a6c5ffe9c4
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-19 18:38:43 +0530

    update

--- Practice1.java
+++ Practice1.java
@@ -9,12 +9,23 @@ public class Practice1 {
     }
 
     public static void Pattern2() {
-
+        int a = 4;
+        for (int k = 1; k <= 4; k++) {
+            for (int i = 1; i <= (a - i - 1); i++) {
+                System.out.println();
+            }
+        for (int j = 1; j <= k; j++) {
+            System.out.println("* ");
+        }
+        System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
 
         // Pattern Reverse
-        Pattern1();
+        // Pattern1();
+
+        Pattern2();
     }
 }
