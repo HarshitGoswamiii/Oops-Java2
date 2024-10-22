69969d64888c25eaf7e8b91fc959ed68acff1492
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-22 20:20:46 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -42,12 +42,12 @@ class patterning {
 
     public void pattern5() { // Inverted Left Half Pyramid
         int a = 4;
-        for (int i = 1; i <= 4; i++) {
-            for (int b = 1; b < (a - i - 1); b++) {
-                System.out.print("* ");
-            }
-            for (int j = 1; j <= 4; j++) {
+        for (int i = 1; i <= a; i++) {
+            for (int b = 1; b <= (a - i); b++) {
                 System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
             }
             System.out.println(" ");
         }
