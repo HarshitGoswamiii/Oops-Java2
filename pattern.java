45554a80f2062408df56230f400d2702235e4675
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-22 20:08:29 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -31,7 +31,7 @@ class patterning {
         }
     }
 
-    public void pattern4() {
+    public void pattern4() { // Inverted Half Triangle
         for (int i = 4; i > 0; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
