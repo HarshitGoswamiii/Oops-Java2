83cb0fdfdc7ff74f0bd15bfd3b1c405e93cd0292
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-22 20:48:54 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -72,9 +72,11 @@ class patterning {
     }
 
     public void pattern8() { // Floyd Triangle
+        int num = 1;
         for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 15; j++) {
-                System.out.print(j);
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num);
+                num++;
             }
             System.out.println();
         }
