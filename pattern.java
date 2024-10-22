f9026ea5a7c7908dfb6d2ba9b243557097c2c0e8
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-22 20:41:18 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -63,8 +63,8 @@ class patterning {
     }
 
     public void pattern7() { // Inverted Half Pyramid with Numbers
-        for (int i = 1; i <= 5; i++) {
-            for (int j = i; j >= 1; j--) {
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
