0e35e36c8be2d3c22c64095626f7fb94461a5b9f
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-22 20:57:46 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -86,9 +86,9 @@ class patterning {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
                 if ((i + j) % 2 == 0) {
-                    System.out.print("1");
+                    System.out.print("1 ");
                 } else if ((i + j) % 2 == 1) {
-                    System.out.print("0");
+                    System.out.print("0 ");
                 }
             }
             System.out.println();
