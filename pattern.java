da23b573a288a7ade112702533ae5d2566bad031
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-22 19:40:26 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -1,16 +1,30 @@
-class patterning{
-    public void pattern1(){
+class patterning {
+
+    public void pattern1() { // Rectangle Pattern
         for (int i = 1; i <= 4; i++) {
-            for(int j =1 ; j<=4;j++){
+            for (int j = 1; j <= 4; j++) {
                 System.out.print("* ");
             }
-        System.out.println();
+            System.out.println();
+        }
+    }
+
+    public void pattern2() { // Hollow Rectangle
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                if (i == 1 || j == 1 || i == 4 || j == 4) {
+                    System.out.print("* ");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
         }
     }
 }
 
-
 public class pattern {
     public static void main(String[] args) {
+        patterning ptr = new patterning();
+        ptr.pattern2();
     }
 }
