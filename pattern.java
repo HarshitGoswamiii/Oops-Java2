ddb3afd6d9d730e62b0097c8cdbdedadab2687b2
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-22 20:57:26 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -83,7 +83,16 @@ class patterning {
     }
 
     public void pattern9() { // 0 1 Triangle
-
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else if ((i + j) % 2 == 1) {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
     }
 }
 
