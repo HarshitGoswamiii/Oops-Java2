04828393209199d53c490fa5cd9aa97b1630afad
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-23 20:25:40 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -181,20 +181,21 @@ class patterning {
                 System.out.print(" ");
             }
             // Pyramid 1
-
             for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
         for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
+            // Spaces
+            for (int j = 1; j <= n - 1; j++) {
                 System.out.print(" ");
             }
             // Pyramid 2
-            for (int j = 1; j >= 1; j++) {
+            for (int j = 1; j <= 2 * n - 1; j++) {
                 System.out.print("* ");
             }
+            System.out.println();
         }
     }
 }
