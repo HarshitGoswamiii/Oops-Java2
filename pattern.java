a847754f41ac39a46acc3f42355baae5f4b3b084
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-23 18:34:57 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -107,6 +107,7 @@ class patterning {
             for (int j = 1; j <= i; j++) { // For Printing stars
                 System.out.print("* ");
             }
+            System.out.println();
         }
     }
 }
