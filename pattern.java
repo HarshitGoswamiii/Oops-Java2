9b10423e9a8e89dff0b319f0f4db396e2b06ebcf
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-23 20:10:55 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -174,11 +174,11 @@ class patterning {
     }
 
     public void pattern14() { // Diamond Pattern
-        for (int i = 4; i > 0; i--) {
+        for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
