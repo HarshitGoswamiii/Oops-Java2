dd79dc5d1f5607798a24ad2f4ef2ef4f778cb238
Harshit Goswami <harshitgoswami244@gmail.com>
2024-10-23 20:05:10 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -158,7 +158,7 @@ class patterning {
         int n = 5;
         for (int i = 1; i <= n; i++) {
             // Spaces
-            for (int j = 1; j <= n - 1; j++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
             // Ist Half
